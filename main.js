d8eed73c8a3ad4e97f434b6968b1db7e26a95704
anaesposito <anaclaraesposito@gmail.com>
2020-12-29 21:02:23 -0300

    function createGrid created

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-const items = ['🐠','🐬','🧜‍♀️','🐙','🐡','🦞']
+// -------------------------------------🐠 Variables JS
+const itemSize = 50;
+const seaCreaturesArray = ["🐠", "🐬", "🧜‍♀️", "🐙", "🐡", "🦞"];
+const list = [];
+const items = "";
+
+// -------------------------------------🐠 Variables DOM
+const grid = document.querySelector(".grid");
+
+// -------------------------------------🐠
+
+const getRandomNumber = (seaCreaturesArray) => {
+  let arrayLength = seaCreaturesArray.length;
+  return Math.floor(Math.random() * arrayLength);
+};
+const getRandomItems = (seaCreaturesArray) => {
+  return seaCreaturesArray[getRandomNumber(seaCreaturesArray)];
+};
+
+const createGrid = (width, height) => {
+  const gridWidth = itemSize * width;
+  grid.style.width = `${gridWidth}px`;
+
+  for (let i = 0; i < width; i++) {
+    listOfItems[i] = [];
+    for (let j = 0; j < height; j++) {
+      listOfItems[i][j] = getRandomItems(seaCreaturesArray);
+    }
+  }
+
+  grid.innerHTML = "";
+  for (let i = 0; i < listOfItems.length; i++) {
+    for (let j = 0; j < listOfItems[i].length; j++) {
+      items = getRandomItems(seaCreaturesArray);
+      listOfItems[i][j] = items;
+      grid.innerHTML += `<div data-x="${i}" data-y="${j}"> ${items}</div>`;
+    }
+  }
+
+  return grid;
+};
